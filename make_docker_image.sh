b672ee887cb7fbe2965b49ceade38ae36ab93c06
gyeonghyun kim <gyeonghyun.kimm@gmail.com>
2023-12-01 00:05:27 +0900

    [CHORE] set docker image build

--- make_docker_image.sh
+++ make_docker_image.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/bash
-image_name="$1"
-./mvnw clean install
+profile="$1"
+image_name="$2"
+
+./mvnw clean install -P"$profile" 
 if [ $? -eq 0 ]; then
-	echo "Maven 빌드 성공, 도커 이미지를 생성합니다."
-	docker buildx build --platform=linux/amd64,linux/arm64 -t "$image_name" --load .
-else
-	echo "Maven 빌드 실패!"
+	docker login -u ttogether
+	if [ $? -eq 0]; then
+		echo "Maven 빌드 성공, 도커 이미지를 생성하고 도커 허브에 push 합니다."
+		docker buildx create --name multi-platform-build --driver docker-container --bootstrap --use
+		docker buildx build --platform=linux/amd64,linux/arm64 -t "$image_name" --push .
+	else
+		echo "docker login 실패"
+	fi
+echo "Maven 빌드 실패!"
 fi
